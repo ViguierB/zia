8d839da8b7041fac5787db01d36854018450ac5a
Viguier Benjamin <benjamin.viguier@epitech.eu>
2019-02-28 21:17:23 +0100

    php post ready

--- srcs/modules/PhpCgi/Module.cpp
+++ srcs/modules/PhpCgi/Module.cpp
@@ -24,6 +24,9 @@ private:
 	inline void		_onHandleRequest(zany::Pipeline::Instance &i);
 	inline void		_onDataReady(zany::Pipeline::Instance &i);
 	inline void		_onHandleResponse(zany::Pipeline::Instance &i);
+	inline void		_beforeCloseSocket(zany::Pipeline::Instance &i);
+
+	inline boost::process::environment		_fillCgiEnv(zany::Pipeline::Instance &i);
 };
 
 void	PhpCgiModule::init() {
@@ -35,6 +38,35 @@ void	PhpCgiModule::init() {
 
 	garbage << this->master->getPipeline().getHookSet<zany::Pipeline::Hooks::ON_HANDLE_RESPONSE>()
 		.addTask<zany::Pipeline::Priority::MEDIUM>(std::bind(&PhpCgiModule::_onHandleResponse, this, std::placeholders::_1));
+
+	garbage << this->master->getPipeline().getHookSet<zany::Pipeline::Hooks::BEFORE_CLOSE_SOCKET>()
+		.addTask<zany::Pipeline::Priority::MEDIUM>(std::bind(&PhpCgiModule::_beforeCloseSocket, this, std::placeholders::_1));
+}
+
+boost::process::environment		PhpCgiModule::_fillCgiEnv(zany::Pipeline::Instance &inst) {
+	constexpr std::pair<char const *, char const *> map[] = {
+		{ "CONTENT_LENGTH", "content-length" },
+		{ "CONTENT_TYPE", "content-type" }
+	};
+	constexpr auto size = sizeof(map) / sizeof(*map);
+
+	auto env = boost::this_process::environment();
+
+	env["SERVER_SOFTWARE"] = "Zia Server -- PCinc";
+	env["SERVER_PROTOCOL"] = inst.request.protocol + "/" + inst.request.protocolVersion;
+	env["SERVER_PORT"] = std::to_string(inst.request.port);
+	env["SERVER_NAME"] = inst.request.host;
+	env["REQUEST_METHOD"] = inst.request.methodString;
+	env["QUERY_STRING"] = inst.request.fullQuery;
+	env["REDIRECT_STATUS"] = "CGI";
+	env["SCRIPT_FILENAME"] = boost::filesystem::current_path().string() + "/" + inst.request.path;
+
+	for (std::size_t i = 0; i < size; ++i) {
+		auto it = inst.request.headers.find(map[i].second);
+		if (it == inst.request.headers.end()) continue;
+		env[map[i].first] = *it->second;
+	}
+	return env;
 }
 
 void	PhpCgiModule::_onHandleRequest(zany::Pipeline::Instance &i) {
@@ -49,23 +81,61 @@ void	PhpCgiModule::_onHandleRequest(zany::Pipeline::Instance &i) {
 		#endif
 			;
 	i.writerID = getUniqueId();
-	auto &is = (i.properties["php stream"] = zany::Property::make<boost ::process::ipstream>()).get<boost::process::ipstream>();
-	i.properties["php child"] = zany::Property::make<boost::process::child>(cgiPath, i.request.path, boost::process::std_out > is);
+	auto &ins = (i.properties["php_cin"] = zany::Property::make<boost::process::opstream>()).get<boost::process::opstream>();
+	auto &outs = (i.properties["php_cout"] = zany::Property::make<boost::process::ipstream>()).get<boost::process::ipstream>();
+	auto &errs = (i.properties["php_cerr"] = zany::Property::make<boost::process::ipstream>()).get<boost::process::ipstream>();
+	i.properties["php_cgi"] = zany::Property::make<boost::process::child>(
+		cgiPath,
+		i.request.path,
+		boost::process::std_in < ins,
+		boost::process::std_out > outs,
+		boost::process::std_err > errs,
+		_fillCgiEnv(i)
+	);
+
 }
 
 void	PhpCgiModule::_onDataReady(zany::Pipeline::Instance &i) {
 	if (i.writerID != getUniqueId() || i.request.method != zany::HttpRequest::RequestMethods::POST)
 		return;
-	i.request.headers.find("content_length");
+	auto it = i.request.headers.find("content-length");
+	if (it == i.request.headers.end() && it->second.isNumber()) {
+		i.response.status = 400;
+		i.writerID = 0;
+		return;
+	}
+
+	thread_local char	buffer[1024];
+	std::size_t			contentlen = static_cast<std::size_t>(it->second.getNumber());
+	std::streamsize		sread;
+	auto				&phpostream = i.properties["php_cin"].get<boost::process::opstream>();
+
+
+	while (contentlen > 0) {
+		sread = i.connection->stream().readsome(
+			buffer,
+			sizeof(buffer) <= contentlen
+				? sizeof(buffer)
+				: contentlen
+		);
+		
+		phpostream.write(buffer, sread);
+		contentlen -= sread;
+	}
+	phpostream.flush();
 }
 
 void	PhpCgiModule::_onHandleResponse(zany::Pipeline::Instance &i) {
 	if (i.writerID != getUniqueId())
 		return;
 
-	auto &stm = i.properties["php stream"].get<boost::process::ipstream>();
+	auto &stm = i.properties["php_cout"].get<boost::process::ipstream>();
 
 	i.connection->stream() << stm.rdbuf();
+}
+
+void	PhpCgiModule::_beforeCloseSocket(zany::Pipeline::Instance &i) {
+	i.properties["php_cgi"].get<boost::process::child>().wait();
 }
 
 }
